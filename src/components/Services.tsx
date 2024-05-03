871755a83dbd2faa2f65528569c5f61db542881d
Maryam Safdar <139070864+maryamsafdar@users.noreply.github.com>
2024-05-03 06:37:33 -0700

    commit

--- src/components/Services.tsx
+++ src/components/Services.tsx
@@ -1,19 +1,17 @@
 import Link from "next/link";
 import Service from "./Service";
 
-
 const Services = () => {
   return (
-     <div id="Services" className="flex text-white min-h-full flex-col items-center mt-4">
-   
-    <p className="flex text-4xl mb-8 mt-8 justify-center text-white font-bold">Services</p>
-    <div className="flex desktop:flex-row mobile:flex-col mobile:items-center flex-grow justify-around text-center">
-      <Service hl="Single Page Applications" desc="Development of responsive web apps such as portfolios, landingpages and dashboards" img="/icons/1.svg" />
-      <Service hl="API Integration" desc="Seamless integration into your solution for dynamic content delivery" img="/icons/2.svg" />
-      <Service hl="SEO Optimization" desc="Enhancing visibility and rankings of your web application" img="/icons/3.svg" />
+    <div id="Services" className="flex text-white min-h-full flex-col items-center mt-4">
+      <p className="text-4xl mb-8 mt-8 text-white font-bold text-center">Services</p>
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-8 justify-items-center">
+        <Service hl="Single Page Applications" desc="Development of responsive web apps such as portfolios, landing pages, and dashboards" img="/icons/1.svg" />
+        <Service hl="API Integration" desc="Seamless integration into your solution for dynamic content delivery" img="/icons/2.svg" />
+        <Service hl="SEO Optimization" desc="Enhancing visibility and rankings of your web application" img="/icons/3.svg" />
+      </div>
     </div>
-    
-  </div>);
+  );
 }
 
 export default Services;
