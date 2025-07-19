4bb514a273de6163cd3d9e6c154da1ecbbfac2f3
Maryam Safdar <139070864+maryamsafdar@users.noreply.github.com>
2025-07-19 16:20:26 +0500

    commit

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -17,31 +17,33 @@ const HeroSection = () => {
         >
           <h1 className="text-white mb-4 text-4xl sm:text-3xl lg:text-5xl lg:leading-normal font-extrabold">
             <span className="text-transparent bg-clip-text bg-gradient-to-r from-primary-400 to-secondary-600">
-              Hello, I&apos;m{" "} Maryam
-              <br></br>
-             <b className="text-white text-3xl sm:text-2xl lg:text-4xl"> a fullstack developer specializing in web applications with </b> 
-
-             <TypeAnimation
-              className="text-teal"
-              sequence={[
-                "Next.js",
-                1000,
-                "Typescript",
-                1000,
-                "Tailwind CSS",
-                1000,
-                "React.js",
-                1000,
-              ]}
-              wrapper="span"
-              speed={50}
-              repeat={Infinity}
-            />
+              Hello, I&apos;m Maryam
+              <br />
+              <b className="text-white text-3xl sm:text-2xl lg:text-4xl">
+                Full Stack & AI Developer skilled in
+              </b>
+              <br />
+              <TypeAnimation
+                className="text-teal"
+                sequence={[
+                  "Next.js",
+                  1000,
+                  "TypeScript",
+                  1000,
+                  "LangChain & LangGraph",
+                  1000,
+                  "Python",
+                  1000,
+                  "Fastapi",
+                  1000,
+                ]}
+                wrapper="span"
+                speed={50}
+                repeat={Infinity}
+              />
             </span>
-            
-           
           </h1>
-          
+
           <div>
             <Link
               href="/#contact"
@@ -49,20 +51,21 @@ const HeroSection = () => {
             >
               Hire Me
             </Link>
-            <a
-  href="/Resume.pdf"
-  download
-  target="_blank"
-  rel="noopener noreferrer"
-  className="px-1 inline-block py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-primary-500 to-secondary-500 hover:bg-slate-800 text-white mt-3"
->
-  <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">
-    Download CV
-  </span>
-</a>
 
+            <a
+              href="/Resume.pdf"
+              download
+              target="_blank"
+              rel="noopener noreferrer"
+              className="px-1 inline-block py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-primary-500 to-secondary-500 hover:bg-slate-800 text-white mt-3"
+            >
+              <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">
+                Download CV
+              </span>
+            </a>
           </div>
         </motion.div>
+
         <motion.div
           initial={{ opacity: 0, scale: 0.5 }}
           animate={{ opacity: 1, scale: 1 }}
